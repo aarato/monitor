0a57cf3028bf708681c4d30235946f4e8c7c0176
aarato <ucipass@gmail.com>
2025-01-05 13:37:29 -0600

    initial commit

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ sudo apt-get install -y \
 
 # Add Docker’s official GPG key
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg --yes
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor | sudo tee /etc/apt/keyrings/docker.gpg > /dev/null
 
 # Set up the stable repository
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
